cdc5cd3f8f43da8786f58c3195bf5432ef66379a
James Alberto Castrillon Gonzalez <121170120+jcastrillon1992@users.noreply.github.com>
2024-06-02 19:59:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+//fix para _dirname
+import path, { dirname } from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+//server
+const app = express();
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en puerto",app.get("port"));
+
+//Configuracion
+app.use(express.static(__dirname + "/public"));
+
+
+
+//Rutas
+app.get("/",(req,res)=> res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register",(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
